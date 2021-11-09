e23de2fe65effc8a59e745380e4121243c4a2a5e
Jacob Chapman <jacob.chapman@loyalty.co.nz>
2021-11-09 14:48:07 +1300

    Add two tests - Valid and Invalid Card Numbers

--- spec/cardVerification_spec.rb
+++ spec/cardVerification_spec.rb
@@ -1,9 +1,19 @@
 require './Program'
 describe Card, "#valid" do
-    context "Valid Codes" do
-        it "Is a valid code" do
-            card = Card.new("6014")
+    it "Check that valid Cards are marked as Valid" do
+        trueValues = ["60141016700078611", "6014111100033006", "6014709045001234", "6014352700000140", "6014355526000020"]
+
+        trueValues.each {|cardNumber|
+            card = Card.new(cardNumber)
+            expect(card.valid).to eq true
+        }
+    end
+    it "Checks that invalid Cards are marked Invalid" do
+        falseValues = ["6014152705006141", "6014 3555 2900 0028", "6013111111111111"]
+
+        falseValues.each {|cardNumber|
+            card = Card.new(cardNumber)
             expect(card.valid).to eq false
-        end
+        }
     end
 end
